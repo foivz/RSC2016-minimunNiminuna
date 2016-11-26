82693ebc4215a08c75054a982cfd7a07674a088c
hhrvoic <koprivnjak.marko@hotmail.com>
2016-11-26 11:59:55 +0100

    initial project

---
+++ ios/RSC/Pods/Target Support Files/Pods-RSC/Pods-RSC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RSC : NSObject
+@end
+@implementation PodsDummy_Pods_RSC
+@end
---
+++ ios/RSC/RSC/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RSC
+//
+//  Created by MTLab on 24/11/2016.
+//  Copyright © 2016 minimun. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    override func viewDidLoad() {
+                super.viewDidLoad()
+            
+        }
+    
+}
